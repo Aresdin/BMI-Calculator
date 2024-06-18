0786ae52ed043d639384a94a641ae8f948292c34
Mohamed Tarek <mohamedtarek9698@gmail.com>
2024-06-19 00:41:15 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+    throw UnimplementedError();
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:  AppBar(
+        title: Center(child: Text('BMI CALCULATOR')),
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add),
+        ),
+      );
+  }
+}
