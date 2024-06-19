76bd472cfdbb3a5d873e107b503941a5876a1b12
Mohamed Tarek <mohamedtarek9698@gmail.com>
2024-06-19 03:01:39 +0300

    Update main.dart
    Co-Authored-By: Mohamed Tarek <55474352+aresdin@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class _InputPageState extends State<InputPage> {
         title: Center(child: Text('BMI CALCULATOR')),
       ),
       body: Center(
-        child: Text('Hiiiiiiiiiii',
+        child: Text('Hiiiiiiiiiii\ we will rock',
             style: TextStyle(
               fontSize: 20,
               color: Colors.white,
